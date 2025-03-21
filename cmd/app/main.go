d973db3ba3e2891573c878b4f2d21a5fa4d6ea98
Дмитрий Елагин <dmitrijelagin@MacBook-Air-Dmitrij.local>
2025-03-21 19:28:12 +0900

    task 9

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"newproject/internal/database"
 	"newproject/internal/handlers"
 	"newproject/internal/taskService"
 	"newproject/internal/userService"
 	"newproject/internal/web/tasks"
 	"newproject/internal/web/users"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -15,30 +17,42 @@ import (
 
 func main() {
 	database.InitDB()
-	database.DB.AutoMigrate(&userService.User{})
+	if err := database.DB.AutoMigrate(&userService.User{}, &taskService.Task{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
 
-	// Инициализируем echo
 	e := echo.New()
-
-	// используем Logger и Recover
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	repo := taskService.NewTaskRepository(database.DB)
-	service := taskService.NewService(repo)
-
-	handler := handlers.NewHandler(service)
-
+	taskRepo := taskService.NewTaskRepository(database.DB)
 	userRepo := userService.NewUserRepository(database.DB)
-	userService := userService.NewUserService(*userRepo)
+
+	taskService := taskService.NewTaskService(taskRepo)
+	userService := userService.NewUserService(userRepo, taskService)
+
+	taskHandler := handlers.NewTaskHandler(taskService, userService)
 	userHandler := handlers.NewUserHandler(userService)
 
 	userStrictHandler := users.NewStrictHandler(userHandler, nil)
 	users.RegisterHandlers(e, userStrictHandler)
 
-	// Прикол для работы в echo. Передаем и регистрируем хендлер в echo
-	strictHandler := tasks.NewStrictHandler(handler, nil) // тут будет ошибка
-	tasks.RegisterHandlers(e, strictHandler)
+	taskStrictHandler := tasks.NewStrictHandler(taskHandler, nil)
+	tasks.RegisterHandlers(e, taskStrictHandler)
+
+	e.GET("/users/:user_id/tasks", func(c echo.Context) error {
+		userID, err := strconv.Atoi(c.Param("user_id"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid user ID"})
+		}
+
+		tasks, err := taskService.GetTasksByUserID(uint(userID))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+		}
+
+		return c.JSON(http.StatusOK, tasks)
+	})
 
 	if err := e.Start(":8080"); err != nil {
 		log.Fatalf("failed to start with err: %v", err)
